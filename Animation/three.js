bcd4f871dbed747674da6ad83c0c2a416e34fce6
KevenMartin <87453981+KevenMartin@users.noreply.github.com>
2023-01-11 17:10:45 -0500

    sections added

--- Animation/three.js
+++ Animation/three.js
@@ -1,98 +1,133 @@
-
 let scene, camera, renderer, stars, starGeo;
 
 function init() {
-  //Create Scene object
-  scene = new THREE.Scene();
+	//Create Scene object
+	scene = new THREE.Scene();
 
-  //setup camera facing upward
-  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 1;
-  camera.rotation.x = Math.PI / 2;
+	//setup camera facing upward
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.z = 1;
+	camera.rotation.x = Math.PI / 2;
 
-  //setup renderer
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+	//setup renderer
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
-  starGeo = new THREE.Geometry();
-  for (let i = 0; i < 6000; i++) {
-    star = new THREE.Vector3(Math.random() * 600 - 300, Math.random() * 600 - 300, Math.random() * 600 - 300);
-    star.velocity = 0;
-    star.acceleration = 0.005;
-    starGeo.vertices.push(star);
-  }
+	blueStarShape = new THREE.Geometry();
+	for (let i = 0; i < 6000; i++) {
+		blueStar = new THREE.Vector3(Math.random() * 600 - 300, Math.random() * 600 - 300, Math.random() * 600 - 300);
+		blueStar.velocity = 0;
+		blueStar.acceleration = 0.005;
+		blueStarShape.vertices.push(blueStar);
+	}
 
-  starGeoGreen = new THREE.Geometry();
-  for (let i = 0; i < 3000; i++) {
-    star = new THREE.Vector3(Math.random() * 600 - 300, Math.random() * 600 - 300, Math.random() * 600 - 300);
-    star.velocity = 0;
-    star.acceleration = 0.005;
-    starGeoGreen.vertices.push(star);
-  }
+	redStarShape = new THREE.Geometry();
+	for (let i = 0; i < 3000; i++) {
+		redStar = new THREE.Vector3(Math.random() * 600 - 300, Math.random() * 600 - 300, Math.random() * 600 - 300);
+		redStar.velocity = 0;
+		redStar.acceleration = 0.003;
+		redStarShape.vertices.push(redStar);
+	}
 
-  let sprite = new THREE.TextureLoader().load("./images/star.png");
-  let starMaterial = new THREE.PointsMaterial({
-    color: 0xbae7ff,
-    size: 0.7,
-    map: sprite,
-  });
+	yellowStarShape = new THREE.Geometry();
+	for (let i = 0; i < 1000; i++) {
+		yellowStar = new THREE.Vector3(Math.random() * 600 - 300, Math.random() * 600 - 300, Math.random() * 600 - 300);
+		yellowStar.velocity = 0;
+		yellowStar.acceleration = 0.001;
+		yellowStarShape.vertices.push(yellowStar);
+	}
 
-  let greenSprite = new THREE.TextureLoader().load("./images/star.png");
-  let starMaterial1 = new THREE.PointsMaterial({
-    color: 0xff8ac1,
-    size: 0.1,
-    map: greenSprite,
-  });
+	let blueSprite = new THREE.TextureLoader().load("./images/star.png");
+	let blueStarDetails = new THREE.PointsMaterial({
+		color: 0xbae7ff,
+		size: 0.7,
+		map: blueSprite,
+	});
 
-  stars = new THREE.Points(starGeo, starMaterial);
-  stars2 = new THREE.Points(starGeoGreen, starMaterial1);
+	let redSprite = new THREE.TextureLoader().load("./images/star.png");
+	let redStarDetails = new THREE.PointsMaterial({
+		color: 0xff8ac1,
+		size: 0.5,
+		map: redSprite,
+	});
 
-  scene.add(stars);
-  scene.add(stars2);
+	let yellowSprite = new THREE.TextureLoader().load("./images/star.png");
+	let yellowStarDetails = new THREE.PointsMaterial({
+		color: 0xfff35d,
+		size: 1,
+		map: yellowSprite,
+	});
 
-  window.addEventListener("resize", onWindowResize, false);
+	blueStars = new THREE.Points(blueStarShape, blueStarDetails);
+	redStars = new THREE.Points(redStarShape, redStarDetails);
+	yellowStars = new THREE.Points(yellowStarShape, yellowStarDetails);
 
-  animate();
-  animateGreen();
+	scene.add(blueStars);
+	scene.add(redStars);
+	scene.add(yellowStars);
+
+	window.addEventListener("resize", onWindowResize, false);
+
+	animateBlueStars();
+	animateRedStars();
+	animateYellowStars();
 }
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function animate() {
-  starGeo.vertices.forEach((p) => {
-    p.velocity += p.acceleration;
-    p.y -= p.velocity;
+function animateBlueStars() {
+	blueStarShape.vertices.forEach((p) => {
+		p.velocity += p.acceleration;
+		p.y -= p.velocity;
 
-    if (p.y < -200) {
-      p.y = 200;
-      p.velocity = 0;
-    }
-  });
-  starGeo.verticesNeedUpdate = true;
-  stars.rotation.y += 0.0;
+		if (p.y < -200) {
+			p.y = 200;
+			p.velocity = 0;
+		}
+	});
+	blueStarShape.verticesNeedUpdate = true;
+	blueStars.rotation.y += 0.0;
 
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+	requestAnimationFrame(animateBlueStars);
 }
 
-function animateGreen() {
-  starGeoGreen.vertices.forEach((p) => {
-    p.velocity += p.acceleration;
-    p.y -= p.velocity;
+function animateRedStars() {
+	redStarShape.vertices.forEach((p) => {
+		p.velocity += p.acceleration;
+		p.y -= p.velocity;
 
-    if (p.y < -200) {
-      p.y = 200;
-      p.velocity = 0;
-    }
-  });
-  starGeoGreen.verticesNeedUpdate = true;
-  stars.rotation.y += 0.0;
+		if (p.y < -200) {
+			p.y = 200;
+			p.velocity = 0;
+		}
+	});
+	redStarShape.verticesNeedUpdate = true;
+	redStars.rotation.y += 0.0;
 
-  renderer.render(scene, camera);
-  requestAnimationFrame(animateGreen);
+	renderer.render(scene, camera);
+	requestAnimationFrame(animateRedStars);
 }
+
+function animateYellowStars() {
+	yellowStarShape.vertices.forEach((p) => {
+		p.velocity += p.acceleration;
+		p.y -= p.velocity;
+
+		if (p.y < -200) {
+			p.y = 200;
+			p.velocity = 0;
+		}
+	});
+	yellowStarShape.verticesNeedUpdate = true;
+	yellowStars.rotation.y += 0.0;
+
+	renderer.render(scene, camera);
+	requestAnimationFrame(animateYellowStars);
+}
+
 init();
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let text = ["Bonjour", "Hi", "kwei", "Hola", "Guten Tag", "ni hao", "Salam", "privet", "konnichiwa"];
+/* let text = ["Bonjour", "Hi", "kwei", "Hola", "Guten Tag", "ni hao", "Salam", "privet", "konnichiwa"];
 let counter = 1;
 let elem = document.getElementById("word");
 let inst = setInterval(change, 1250);
@@ -11,3 +11,44 @@ function change() {
 		// clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
 	}
 }
+
+document.body.addEventListener("mousemove", (e) => {
+	gsap.to(".circle", {
+		x: e.pageX,
+		y: e.pageY,
+	});
+});
+
+let myDiv = document.getElementById("shape");
+//Detect touch device
+function isTouchDevice() {
+	try {
+		//We try to create TouchEvent. It would fail for desktops and throw error
+		document.createEvent("TouchEvent");
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+const move = (e) => {
+	//Try, catch to avoid any errors for touch screens (Error thrown when user doesn't move his finger)
+	try {
+		//PageX and PageY return the position of client's cursor from top left of screen
+		var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+		var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+	} catch (e) {}
+	//set left and top of div based on mouse position
+	myDiv.style.left = x - 50 + "px";
+	myDiv.style.top = y - 50 + "px";
+};
+//For mouse
+document.addEventListener("mousemove", (e) => {
+	move(e);
+});
+//For touch
+document.addEventListener("touchmove", (e) => {
+	move(e);
+});
+ */
+
