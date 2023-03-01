530f894936d7b6f0319d3aff71e2cbde541cf860
KevenMartin <87453981+KevenMartin@users.noreply.github.com>
2023-03-01 14:23:45 -0500

    removed touch device for shape

--- script.js
+++ script.js
@@ -19,29 +19,15 @@ window.addEventListener("scroll", function (e) {
 });
 
 let myDiv = document.getElementById("shape");
-function isTouchDevice() {
-	try {
-		document.createEvent("TouchEvent");
-		return true;
-	} catch (e) {
-		return false;
-	}
-}
 
 const move = (e) => {
-	try {
-		var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
-		var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
-	} catch (e) {}
+	var x = e.pageX;
+	var y = e.pageY;
 	myDiv.style.left = x - 50 + "px";
 	myDiv.style.top = y - 50 + "px";
 };
-//For mouse
+
 document.addEventListener("mousemove", (e) => {
-	move(e);
-});
-//For touch
-document.addEventListener("touchmove", (e) => {
 	move(e);
 });
 
